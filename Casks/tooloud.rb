6ccb9408b93f3c9d1254b63ddfa310b13e5a2d84
Patrick Oberherr <poberherr@users.noreply.github.com>
2026-03-23 10:53:33 +0100

    fix: point cask URL to public homebrew-tooloud releases

--- Casks/tooloud.rb
+++ Casks/tooloud.rb
@@ -2,7 +2,7 @@ cask "tooloud" do
   version "1.0.0"
   sha256 "e44d1999e0a4f8ea541970de902be6bf0024c714a33fb7a4a3e61aef0e465a24"
 
-  url "https://github.com/poberherr/tooLoud/releases/download/v#{version}/tooLoud.dmg"
+  url "https://github.com/poberherr/homebrew-tooloud/releases/download/v#{version}/tooLoud.dmg"
   name "tooLoud"
   desc "Menu bar noise monitor that alerts when you're too loud"
   homepage "https://github.com/poberherr/tooLoud"
